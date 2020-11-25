8dc79695136b7e4f491b2c24d2620ea8ed473b8c
LunaTW <68732727+LunaTW@users.noreply.github.com>
2020-11-25 19:49:03 +0800

    add map,list,object

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
@@ -2,12 +2,18 @@ package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties("mail")
+import java.util.List;
+import java.util.Map;
+
+@ConfigurationProperties(prefix="mail")
 public class MailConfig {
 
     private String hostname;
     private int port;
     private String from;
+    private List<String> defaultRecipients;
+    private Map<String,Boolean> additionalHeaders;
+    private Credentials credentials;
 
     public String getHostname() {
         return hostname;
@@ -33,12 +39,42 @@ public class MailConfig {
         this.from = from;
     }
 
+    public List<String> getDefaultRecipients() {
+        return defaultRecipients;
+    }
+
+    public void setDefaultRecipients(List<String> defaultRecipients) {
+        this.defaultRecipients = defaultRecipients;
+    }
+
+    public Map<String, Boolean> getAddtionalHeaders() {
+        return additionalHeaders;
+    }
+
+    public void setAddtionalHeaders(Map<String, Boolean> addtionalHeaders) {
+        this.additionalHeaders = addtionalHeaders;
+    }
+
+    public Credentials getCredentials() {
+        return credentials;
+    }
+
+    public void setCredentials(Credentials credentials) {
+        this.credentials = credentials;
+    }
+
     @Override
     public String toString() {
         return "MailConfig{" +
                 "hostname='" + hostname + '\'' +
                 ", port=" + port +
                 ", from='" + from + '\'' +
-                '}';
+                '}' + "\n" +
+                "MailRecipients[" +
+                String.join(", ", defaultRecipients)
+                +"]" + "\n" +
+                "MailAdditionalHeaders" +
+                additionalHeaders.toString() + "\n" +
+                credentials;
     }
 }
